8b97bc19e7ea1b91098469a0b8dcc1268470d6de
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-04-03 06:30:53 -0400

    Add keywords

--- packages/gbnf/src/index.ts
+++ packages/gbnf/src/index.ts
@@ -1,3 +1,11 @@
 export { GBNF as default, } from './gbnf.js';
-export { RuleType, type RuleCharExclude, type RuleChar, type RuleEnd, } from './grammar-parser/graph/types.js';
+export {
+  isRange,
+  RuleType,
+  type ReturnRuleValue as Rule,
+  type RuleCharExclude,
+  type RuleChar,
+  type RuleEnd,
+  type Range,
+} from './grammar-parser/graph/types.js';
 export { ParseState, } from './grammar-parser/graph/state.js';
