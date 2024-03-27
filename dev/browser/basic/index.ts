2247318356f29c15bd458200040be3604dbbd857
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-03-27 16:04:34 -0400

    Expose GrammarParser class as a type
    Refactors grammar parser-specific functions and speeds up flat rule fetching

--- dev/browser/basic/index.ts
+++ dev/browser/basic/index.ts
@@ -1,4 +1,4 @@
-import GBNF, { RuleDef, Range, RuleType, } from 'gbnf';
+import GBNF, { Rule, Range, RuleType, } from 'gbnf';
 
 const GrammarParser = GBNF('root ::= "foo"');
 const grammar = new GrammarParser('fo');
@@ -7,5 +7,5 @@ console.log(grammar.rules)
 console.log(RuleType.ALT)
 const r: Range = [1, 2];
 console.log(r);
-const r2w: RuleDef = { type: RuleType.CHAR, value: 1 };
+const r2w: Rule = { type: RuleType.CHAR, value: 1 };
 console.log(r2w);
--- packages/gbnf/src/index.ts
+++ packages/gbnf/src/index.ts
@@ -1,3 +1,4 @@
 export { GBNF as default, } from './gbnf.js';
 export type { Rule, Range, } from './types.js';
 export { RuleType, } from './types.js';
+export { AbstractGrammarParser as GrammarParser, } from './grammar-parser/abstract-grammar-parser.js';
