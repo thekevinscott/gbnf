8de1678d410f30b46fd702bcf4aafd67ee0f9689
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-03-27 21:03:15 -0400

    Tweak build script, fix types for top-level index

--- packages/gbnf/vite.config.ts
+++ packages/gbnf/vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig, } from 'vitest/config';
-import dts from 'vite-plugin-dts';
+// import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
+      entry: 'src/umd.ts',
+      formats: ['umd',],
       fileName: 'index',
-      formats: ['es', 'umd',],
-      name: 'CodeSynth',
+      name: 'GBNF',
     },
     sourcemap: true,
     target: 'esnext',
     minify: true,
   },
-  plugins: [dts(),],
+  // plugins: [dts({ rollupTypes: true, }),],
   test: {
     include: ['**/*.test.ts',],
     globals: true,
