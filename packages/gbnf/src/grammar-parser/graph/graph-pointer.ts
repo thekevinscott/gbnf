0243917bb3c59d9cdb8ff6ba6081001e00b3917d
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-04-01 04:55:22 -0400

    Refactor graph pointer resolution statements

--- packages/gbnf/src/grammar-parser/graph/graph-pointer.ts
+++ packages/gbnf/src/grammar-parser/graph/graph-pointer.ts
@@ -1,13 +1,19 @@
 import { Color, Colorize, } from "./colorize.js";
 import { GraphNode, } from "./graph-node.js";
-import { isRuleEnd, isRuleRef, type PrintOpts, } from "./types.js";
+import { GraphRule, Rule, RuleChar, RuleEnd, RuleRange, RuleRef, isRuleChar, isRuleEnd, isRuleRange, isRuleRef, type PrintOpts, } from "./types.js";
 
-export class GraphPointer {
-  node: GraphNode;
+export type VisibleGraphPointer = GraphPointer<Rule>;
+const isGraphPointerRuleRef = (pointer: GraphPointer): pointer is GraphPointer<RuleRef> => isRuleRef(pointer.rule);
+const isGraphPointerRuleEnd = (pointer: GraphPointer): pointer is GraphPointer<RuleEnd> => isRuleEnd(pointer.rule);
+const isGraphPointerRuleChar = (pointer: GraphPointer): pointer is GraphPointer<RuleChar> => isRuleChar(pointer.rule);
+const isGraphPointerRuleRange = (pointer: GraphPointer): pointer is GraphPointer<RuleRange> => isRuleRange(pointer.rule);
+
+export class GraphPointer<R extends GraphRule = GraphRule> {
+  node: GraphNode<R>;
   parent?: GraphPointer;
   #valid?: boolean;
 
-  constructor(node: GraphNode, parent?: GraphPointer) {
+  constructor(node: GraphNode<R>, parent?: GraphPointer) {
     if (node === undefined) {
       throw new Error('Node is undefined');
     }
@@ -15,8 +21,32 @@ export class GraphPointer {
     this.parent = parent;
   }
 
+  *resolve(resolved = false): IterableIterator<VisibleGraphPointer> {
+    if (isGraphPointerRuleRef(this)) {
+      if (resolved) {
+        const nextPointer = new GraphPointer(this.node.next, this.parent);
+        yield* nextPointer.resolve();
+      } else {
+        for (const node of this.node.rule.nodes) {
+          const nextPointer = new GraphPointer(node, this);
+          yield* nextPointer.resolve();
+        }
+      }
+    } else if (isGraphPointerRuleEnd(this)) {
+      if (!this.parent) {
+        yield this;
+      } else {
+        yield* this.parent.resolve(true);
+      }
+    } else if (isGraphPointerRuleChar(this) || isGraphPointerRuleRange(this)) {
+      yield this;
+    } else {
+      throw new Error(`Unknown rule type: ${this.node.rule.type}`);
+    }
+  }
+
   // method for fetching the next pointer. 
-  *fetchNext(): IterableIterator<GraphPointer> {
+  *fetchNext(): IterableIterator<VisibleGraphPointer> {
     if (this.#valid === false) {
       return;
     }
@@ -32,32 +62,70 @@ export class GraphPointer {
     //   }
     // }
 
-    const node = this.node.next;
-    if (node) {
-      if (isRuleRef(node.rule)) {
-        for (const next of node.rule.nodes) {
-          if (isRuleEnd(next.rule)) {
-            if (this.parent) {
-              yield* this.parent.fetchNext();
-            } else {
-              yield new GraphPointer(next);
-            }
-          } else {
-            yield new GraphPointer(next, new GraphPointer(node, this.parent));
-          }
-        }
-      } else if (isRuleEnd(node.rule)) {
-        if (this.parent) {
-          for (const { node: next, parent, } of this.parent.fetchNext()) {
-            yield new GraphPointer(next, parent);
-          }
-        } else {
-          yield new GraphPointer(node);
-        }
-      } else {
-        yield new GraphPointer(node, this.parent);
+    // if (!isRuleEnd(this.node.rule) && !isRuleRef(this.node.rule)) {
+    // if (isRuleChar(this.node.rule) || isRuleRange(this.node.rule)) {
+    if (isRuleEnd(this.node.rule)) {
+      if (this.parent) {
+        yield* this.parent.fetchNext();
       }
+    } else {
+      const nextPointer = new GraphPointer(this.node.next, this.parent);
+      yield* nextPointer.resolve();
     }
+    //   if (isRuleChar(this.node.next.rule) || isRuleRange(this.node.next.rule)) {
+    //     // 
+    //     const nextPointer = new GraphPointer(this.node.next, this.parent);
+    //     yield* nextPointer.resolve();
+    //   } else if (isRuleRef(this.node.next.rule)) {
+    //     for (const next of this.node.next.rule.nodes) {
+    //       const nextPointer = new GraphPointer(next, this);
+    //       yield* nextPointer.fetchNext();
+    //       // yield nextPointer;
+    //     }
+    //     // for (const next of this.node.next.rule.nodes) {
+    //     //   if (isRuleEnd(next.rule)) {
+    //     //     if (this.parent) {
+    //     //       // yield* this.parent.fetchNext();
+    //     //     } else {
+    //     //       yield new GraphPointer(next);
+    //     //     }
+    //     //   } else {
+    //     //     // yield new GraphPointer(next, new GraphPointer(this.node, this.parent));
+    //     //   }
+    //     // }
+    //   } else if (isRuleEnd(this.node.next.rule)) {
+    //     if (this.parent) {
+    //       yield* this.parent.fetchNext();
+    //     }
+    //   }
+    // // }
+
+    // const node = this.node.next;
+    // if (node) {
+    //   if (isRuleRef(node.rule)) {
+    //     for (const next of node.rule.nodes) {
+    //       if (isRuleEnd(next.rule)) {
+    //         if (this.parent) {
+    //           yield* this.parent.fetchNext();
+    //         } else {
+    //           yield new GraphPointer(next);
+    //         }
+    //       } else {
+    //         yield new GraphPointer(next, new GraphPointer(node, this.parent));
+    //       }
+    //     }
+    //   } else if (isRuleEnd(node.rule)) {
+    //     if (this.parent) {
+    //       for (const { node: next, parent, } of this.parent.fetchNext()) {
+    //         yield new GraphPointer(next, parent);
+    //       }
+    //     } else {
+    //       yield new GraphPointer(node);
+    //     }
+    //   } else {
+    //     yield new GraphPointer(node, this.parent);
+    //   }
+    // }
   }
 
   get rule() {
--- packages/gbnf/src/grammar-parser/graph/graph-pointers-store.ts
+++ packages/gbnf/src/grammar-parser/graph/graph-pointers-store.ts
@@ -1,18 +1,27 @@
-import { GraphPointer, } from "./graph-pointer.js";
+import { GraphPointer, VisibleGraphPointer, } from "./graph-pointer.js";
+import { isRuleEnd, isRuleRef, } from "./types.js";
 
 export class GraphPointersStore {
   keys = new Set<string>();
-  pointers = new Set<GraphPointer>();
+  pointers = new Set<VisibleGraphPointer>();
 
-  add = (pointer: GraphPointer) => {
-    const key = getPointerKey(pointer);
-    if (!this.keys.has(key)) {
-      this.keys.add(key);
-      this.pointers.add(pointer);
+  add = (unresolvedPointer: GraphPointer) => {
+    for (const pointer of unresolvedPointer.resolve()) {
+      if (isRuleRef(pointer.node.rule)) {
+        throw new Error('Encountered a reference rule when building pointers to the graph');
+      }
+      if (isRuleEnd(pointer.node.rule) && !!pointer.parent) {
+        throw new Error('Encountered an ending rule with a parent when building pointers to the graph');
+      }
+      const key = getPointerKey(pointer);
+      if (!this.keys.has(key)) {
+        this.keys.add(key);
+        this.pointers.add(pointer);
+      }
     }
   };
 
-  delete = (pointer: GraphPointer) => {
+  delete = (pointer: VisibleGraphPointer) => {
     this.pointers.delete(pointer);
     const key = getPointerKey(pointer);
     this.keys.delete(key);
@@ -29,7 +38,7 @@ const getPointerKey = ({
     },
   },
   parent,
-}: GraphPointer): string => JSON.stringify({
+}: VisibleGraphPointer): string => JSON.stringify({
   id,
   stackId, pathId, stepId,
   parent: parent ? getPointerKey(parent) : null,
