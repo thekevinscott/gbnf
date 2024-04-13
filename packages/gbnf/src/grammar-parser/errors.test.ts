61a2844550be32cdb1385d5e3d817bed956f5d51
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-04-13 07:07:43 -0400

    Fix error message positioning

---
+++ packages/gbnf/src/grammar-parser/errors.test.ts
@@ -0,0 +1,161 @@
+import {
+  GRAMMAR_PARSER_ERROR_HEADER_MESSAGE,
+  GrammarParseError,
+  INPUT_PARSER_ERROR_HEADER_MESSAGE,
+  InputParseError,
+  buildErrorPosition,
+
+} from './errors.js';
+describe('errors', () => {
+  describe('buildErrorPosition', () => {
+    test.each([
+      [
+        'root ::= "foo"',
+        1,
+        [
+          'root ::= "foo"',
+          ' ^',
+        ],
+      ],
+      [
+        'root ::= "foo"',
+        5,
+        [
+          'root ::= "foo"',
+          '     ^',
+        ],
+      ],
+      // multi line grammars with pos on first line
+      [
+        'aa\\nbb',
+        1,
+        [
+          'aa',
+          ' ^',
+        ],
+      ],
+      // multi line grammars with pos on second line, first character
+      [
+        'aa\\nbb',
+        2,
+        [
+          'aa\\nbb',
+          '^',
+        ],
+      ],
+      // multi line grammars with pos on second line, second character
+      [
+        'aa\\nbb',
+        2 + 1,
+        [
+          'aa\\nbb',
+          ' ^',
+        ],
+      ],
+      // multi line grammars beyond error with pos on second line
+      [
+        'aa\\nbb\\ncc',
+        2 + 1,
+        [
+          'aa\\nbb',
+          ' ^',
+        ],
+      ],
+      // multi line grammars beyond error with pos on third line, first char
+      [
+        'aa\\nbb\\ncc',
+        2 + 2 + 0,
+        [
+          'aa\\nbb\\ncc',
+          '^',
+        ],
+      ],
+      // multi line grammars beyond error with pos on third line, second char
+      [
+        'aa\\nbb\\ncc',
+        2 + 2 + 1,
+        [
+          'aa\\nbb\\ncc',
+          ' ^',
+        ],
+      ],
+      // multi line grammars beyond error with pos on fourth line, first char
+      [
+        'aa\\nbb\\ncc\\ndd',
+        2 + 2 + 2 + 0,
+        [
+          'bb\\ncc\\ndd',
+          '^',
+        ],
+      ],
+      // multi line grammars beyond error with pos on fifth line, second char
+      [
+        'aa\\nbb\\ncc\\ndd\\nee',
+        2 + 2 + 2 + 2 + 1,
+        [
+          'cc\\ndd\\nee',
+          ' ^',
+        ],
+      ],
+    ])('it correctly shows position for %s', (grammar, pos, [grammarOut, posOut,]) => {
+      const result = buildErrorPosition(grammar.split('\\n').join('\n'), pos);
+      expect(result).toEqual([
+        ...grammarOut.split('\\n'),
+        posOut,
+      ]);
+    });
+  });
+
+  describe('GrammarParseError', () => {
+    test('it renders a message', () => {
+      const grammar = 'aa\\nbb\\ncc\\ndd\\nee';
+      const pos = 5;
+      const reason = 'reason';
+      const err = new GrammarParseError(grammar.split('\\n').join('\n'), pos, reason);
+      expect(err.message).toEqual([
+        GRAMMAR_PARSER_ERROR_HEADER_MESSAGE(reason),
+        '',
+        'aa',
+        'bb',
+        'cc',
+        ' ^',
+      ].join('\n'));
+    });
+  });
+
+  describe('InputParseError', () => {
+    test('it renders a message', () => {
+      const input = 'some input';
+      const pos = 1;
+      const err = new InputParseError(input.split('\\n').join('\n'), pos);
+      expect(err.message).toEqual([
+        INPUT_PARSER_ERROR_HEADER_MESSAGE,
+        '',
+        input,
+        ' ^',
+      ].join('\n'));
+    });
+
+    test('it renders a message for code point', () => {
+      const pos = 0;
+      const err = new InputParseError([97], pos);
+      expect(err.message).toEqual([
+        INPUT_PARSER_ERROR_HEADER_MESSAGE,
+        '',
+        'a',
+        '^',
+      ].join('\n'));
+    });
+
+    test('it renders a message for code points', () => {
+      const pos = 2;
+      const err = new InputParseError([97, 98, 99, 100], pos);
+      expect(err.message).toEqual([
+        INPUT_PARSER_ERROR_HEADER_MESSAGE,
+        '',
+        'abcd',
+        '  ^',
+      ].join('\n'));
+    });
+  });
+});
--- packages/gbnf/src/grammar-parser/errors.ts
+++ packages/gbnf/src/grammar-parser/errors.ts
@@ -1,43 +1,59 @@
 import type { ValidInput, } from "./graph/types.js";
 
+export const GRAMMAR_PARSER_ERROR_HEADER_MESSAGE = (reason: string) => `Failed to parse grammar: ${reason}`;
+export const INPUT_PARSER_ERROR_HEADER_MESSAGE = `Failed to parse input string:`;
+export const MAXIMUM_NUMBER_OF_ERROR_LINES_TO_SHOW = 3;
+
+export const buildErrorPosition = (src: string, pos: number): string[] => {
+  const grammarLines = src.split('\n');
+
+  let lineIdx = 0;
+  while (pos > grammarLines[lineIdx].length - 1) {
+    pos -= grammarLines[lineIdx].length;
+    lineIdx += 1;
+  }
+
+  const linesToShow: string[] = [];
+  for (let i = Math.max(0, lineIdx - (MAXIMUM_NUMBER_OF_ERROR_LINES_TO_SHOW - 1)); i <= lineIdx; i++) {
+    linesToShow.push(grammarLines[i]);
+  }
+  return [
+    ...linesToShow,
+    ' '.repeat(pos) + '^',
+  ];
+};
+
 export class GrammarParseError extends Error {
+  grammar: string;
+  pos: number;
+  reason: string;
   constructor(grammar: string, pos: number, reason: string) {
     super([
-      `Failed to parse grammar: ${reason}`,
+      GRAMMAR_PARSER_ERROR_HEADER_MESSAGE(reason),
       '',
-      `${limit(grammar)}`,
-      ' '.repeat(pos) + '^',
-      '',
+      ...buildErrorPosition(grammar, pos),
     ].join('\n'));
     this.name = 'GrammarParseError';
+    this.grammar = grammar;
+    this.reason = reason;
+    this.pos = pos;
   }
 }
 
 export class InputParseError extends Error {
   src: ValidInput;
-  strPos: number;
-  constructor(src: ValidInput, strPos: number) {
+  pos: number;
+  constructor(src: ValidInput, pos: number) {
     super([
       `Failed to parse input string:`,
       '',
-      `${limit(getInputAsString(src))}`,
-      ' '.repeat(strPos) + '^',
-      '',
+      ...buildErrorPosition(getInputAsString(src), pos),
     ].join('\n'));
     this.src = src;
-    this.strPos = strPos;
+    this.pos = pos;
     this.name = 'InputParseError';
   }
 }
-
-const replaceEscapeSequences = (src: string) => {
-  return src;
-  // return src.replace(/\n/g, '\\n').replace(/\t/g, '\\t');
-};
-
-const limit = (str: string, limit: number = 30 * 100) => {
-  return str.length > limit ? `${replaceEscapeSequences(str).substring(0, limit - 3)}...` : replaceEscapeSequences(str);
-};
 
 const getInputAsString = (src: ValidInput) => {
   if (typeof src === 'string') {
