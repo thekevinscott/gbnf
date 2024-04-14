5d50244d8bac475bd76cce3276cddcae958aed7b
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-04-14 19:41:17 -0400

    Update UMD exports

--- packages/gbnfjs/src/umd.ts
+++ packages/gbnfjs/src/umd.ts
@@ -1,5 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { GBNF, } from './gbnf.js';
 import { InternalRuleType, } from './rules-builder/types.js';
+
+import {
+  isRange,
+  RuleType,
+} from './grammar-graph/types.js';
+import { ParseState, } from './grammar-graph/parse-state.js';
+import {
+  InputParseError,
+  GrammarParseError,
+} from './utils/errors.js';
+
 module.exports = GBNF;
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 module.exports.RuleType = InternalRuleType;
+module.exports.isRange = isRange;
+module.exports.RuleType = RuleType;
+module.exports.ParseState = ParseState;
+module.exports.GrammarParseError = GrammarParseError;
+module.exports.InputParseError = InputParseError;
